d9139e30b385982e0d3d97b8dbd808684271cb39
Irvino Djuana <irvino.djuana@gmail.com>
2020-02-17 12:31:14 -0800

    added comments and tests to Q4.8

--- src/data_structures/trees_and_graphs/Question4_8.py
+++ src/data_structures/trees_and_graphs/Question4_8.py
@@ -1,9 +1,11 @@
 
 class Question4_8:
+    # Find first common ancestor of 2 nodes: O(n)
     def first_common_ancestor(self, root, n1, n2):
         code, node = self.DFS(root, n1, n2)
         return node
     
+    # Recursive helper
     def DFS(self, root, n1, n2):
         if not root:
             return 0, None
@@ -12,6 +14,10 @@ class Question4_8:
         c2, res2 = self.DFS(root.right, n1, n2)
         code = c1 + c2
 
+        # Codes: 
+        # (1 = n1 found in branch)
+        # (2 = n2 found in branch)
+        # (4 = FCA found in branch)
         if root == n1:
             if code == 2:
                 return 4, n1
@@ -44,6 +50,7 @@ def main():
     for i in range(9):
         nodes[i] = Node(i)
     
+    # Test finding n5, n6 ancestor = other node
     nodes[0].left = nodes[1]
     nodes[0].right = nodes[2]
     nodes[1].left = nodes[3]
@@ -55,7 +62,16 @@ def main():
 
     Q = Question4_8()
     result = Q.first_common_ancestor(nodes[0], nodes[5], nodes[6])
-    print(result.val)
+    print("Should be 1:", result.val)
+
+    # Test finding ancestor when n5 ancestor of n6
+    nodes[2].left = None
+    nodes[4].right = None
+    nodes[5].right = nodes[7]
+    nodes[7].left = nodes[6]
+
+    result = Q.first_common_ancestor(nodes[0], nodes[5], nodes[6])
+    print("Should be 5:", result.val)
 
 if __name__ == "__main__":
     main()
